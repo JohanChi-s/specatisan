651fd9409bcd2ddebf7474319f98d9fe67891b5e
quanghoangf <quanghoang0016@gmail.com>
2024-05-28 21:36:09 +0700

    chore: deploy test

--- src/app/(main)/dashboard/[workspaceId]/[documentId]/page.tsx
+++ src/app/(main)/dashboard/[workspaceId]/[documentId]/page.tsx
@@ -78,6 +78,7 @@ export default function Document({
             email: user.email, // Add any other relevant information
           }),
         });
+        console.log("response.ok", response);
 
         if (!response.ok) {
           throw new Error("Failed to fetch token");
