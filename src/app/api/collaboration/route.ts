fb3b99ebad44bb0294daf8eab49e7d698536483b
quanghoangf <quanghoang0016@gmail.com>
2024-05-28 21:43:03 +0700

    chore: deploy test

--- src/app/api/collaboration/route.ts
+++ src/app/api/collaboration/route.ts
@@ -6,16 +6,18 @@ const JWT_SECRET = process.env.TIPTAP_COLLAB_SECRET as string;
 export async function POST(req: NextRequest): Promise<Response> {
   try {
     const { userId, email } = await req.json();
+    console.log("userId", userId);
+    console.log("email", email);
 
     // Define the payload object to be encoded in the JWT
     const payload = {
       userId,
       email,
-      exp: Math.floor(Date.now() / 1000) + 60 * 60, // Optional: Set expiration time (1 hour)
     };
 
     // Sign the JWT with the payload and secret
     const jwt = jsonwebtoken.sign(payload, JWT_SECRET);
+    console.log("🚀 ~ POST ~ jwt:", jwt);
 
     // Return the signed JWT as a JSON response
     return new Response(JSON.stringify({ token: jwt }), {
@@ -25,12 +27,16 @@ export async function POST(req: NextRequest): Promise<Response> {
       },
     });
   } catch (error) {
+    console.log("🚀 ~ POST ~ error:", error);
     // Handle any errors that occur during the signing process
-    return new Response(JSON.stringify({ error: "Failed to generate token" }), {
-      status: 500,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    return new Response(
+      JSON.stringify({ error: `Failed to generate token ${error}` }),
+      {
+        status: 500,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
   }
 }
